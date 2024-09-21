fa965891eb8e57a19b0ba9757a8b6c6969b3d2e3
VIOVNL <info@viov.nl>
2024-09-21 12:18:11 +0200

    Base template

---
+++ Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudAnalytic.Models
+{
+    public class AppConfig
+    {
+        public required string Version { get; set; }
+        public bool Production { get; set; }
+        public string? ConnectionString { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,38 @@
-using CloudAnalytic.Components;
 using Radzen;
+using CloudAnalytic.Components;
+using CloudAnalytic.Models;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddHubOptions(options => options.MaximumReceiveMessageSize = 10 * 1024 * 1024);
+
+builder.Services.AddControllers();
 builder.Services.AddRadzenComponents();
+
+builder.Services.AddRadzenCookieThemeService(options =>
+{
+    options.Name = "CloudAnalyticTheme";
+    options.Duration = TimeSpan.FromDays(365);
+});
+builder.Services.Configure<AppConfig>(builder.Configuration.GetSection("AppConfig"));
+builder.Services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<AppConfig>>().Value);
+
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
